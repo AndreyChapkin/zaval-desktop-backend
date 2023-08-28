f4145404d0358aca22dfaae117907e67b37f1da9
andrey <test@test>
2023-08-28 10:51:39 +0200

    + production cors;

--- src/main/kotlin/org/home/zaval/zavalbackend/controller/TodoController.kt
+++ src/main/kotlin/org/home/zaval/zavalbackend/controller/TodoController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/todo")
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin("http://localhost:5173", "http://localhost:3000")
 class TodoController(
     val todoService: TodoService
 ) {
